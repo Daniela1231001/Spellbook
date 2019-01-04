14bd4df59b9040502b3c94b47d3e9c9dc19c0225
Daniela1231001 <43377315+Daniela1231001@users.noreply.github.com>
2019-01-03 21:52:00 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$("#Spell1").click(function() {
+    $("#leviosa").toggle();
+    
+});
+
+$("#Spell2").click(function() {
+    $("#imageexpell").slideToggle();
+
+});
+
+
+$("#Spell3").click(function() {
+    $("#thirdspell").fadeToggle();
+
+});
+
+
+
+
+
+
